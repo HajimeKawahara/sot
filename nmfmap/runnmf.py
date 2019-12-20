61df235c61e3535166a3af0a86534100a3c2c49b
Hajime Kawahara <divrot@gmail.com>
2019-12-20 21:20:00 +0900

    --

--- nmfmap/runnmf.py
+++ nmfmap/runnmf.py
@@ -1,8 +1,7 @@
 import cupy as cp
 import numpy as np
 
-
-def NG_L2MVC_NMF(Ntry,lcall,Win,A0,X0,lam,epsilon):
+def L2_NMF(Ntry,lcall,Win,A0,X0,lamA,lamX,epsilon):
     Y=cp.asarray(lcall)
     W=cp.asarray(Win)
     A=cp.asarray(A0)
@@ -10,81 +9,33 @@ def NG_L2MVC_NMF(Ntry,lcall,Win,A0,X0,lam,epsilon):
     logmetric=[]
     jj=0
     for i in range(0,Ntry):
-        AA=np.sum(A*A)
-        ATA = cp.dot(A.T,A)
-
+        
         #----------------------------------------------------
         if np.mod(i,1000)==0:
             jj=jj+1
+            AA=np.sum(A*A)
+            XX=np.sum(X*X)
             chi2=cp.sum((Y - cp.dot(cp.dot(W,A),X))**2)
-            metric=[i,cp.asnumpy(chi2+lam*AA),cp.asnumpy(chi2),lam*AA]
+            metric=[i,cp.asnumpy(chi2+lamA*AA+lamX*XX),cp.asnumpy(chi2),lamA*AA,lamX*XX]
             logmetric.append(metric)
             #            print(metric,np.sum(A),np.sum(X))
             import terminalplot
             Xn=cp.asnumpy(X)
             bandl=np.array(range(0,len(Xn[0,:])))
+            print(metric)
             terminalplot.plot(list(bandl),list(Xn[np.mod(jj,3),:]))
-
-            if np.mod(i,10000)==0:
-                LogNMF(i,cp.asnumpy(A),cp.asnumpy(X))
-        #----------------------------------------------------
-            
-        Wt = cp.dot(cp.dot(cp.dot(W.T,Y),X.T),ATA)+ epsilon
-        Wb = cp.dot(cp.dot(cp.dot(cp.dot(cp.dot(W.T,W),A),X),X.T),ATA) + lam*cp.linalg.det(ATA)*A + lam*2*cp.dot(A,ATA) + epsilon
-        A = A*(Wt/Wb)
-
-        A = A*(Wt/Wb)
-        #A = cp.dot(cp.diag(1/cp.sum(A[:,:],axis=1)),A)
-        A = cp.dot(cp.diag(1/cp.sum(A[:,:],axis=1)),A)
-        
-        Wt = cp.dot(cp.dot(A.T,W.T),Y)+ epsilon
-        Wb = cp.dot(cp.dot(cp.dot(cp.dot(A.T,W.T),W),A),X)+ epsilon 
-        X = X*(Wt/Wb)
-        #X = cp.dot(cp.diag(1/cp.sum(X[:,:],axis=1)),X)
-        #X = cp.dot(X,cp.diag(1/cp.sum(X[:,:],axis=0)))
-        
-    A=cp.asnumpy(A)
-    X=cp.asnumpy(X)
-    #----------------------------------------------------
-    LogMetricPlot(logmetric)
-    #----------------------------------------------------
-
-    return A, X
-
-
-def L2_NMF(Ntry,lcall,Win,A0,X0,lam,epsilon):
-    Y=cp.asarray(lcall)
-    W=cp.asarray(Win)
-    A=cp.asarray(A0)
-    X=cp.asarray(X0)
-    logmetric=[]
-    jj=0
-    for i in range(0,Ntry):
-        AA=np.sum(A*A)
-        #----------------------------------------------------
-        if np.mod(i,1000)==0:
-            jj=jj+1
-            chi2=cp.sum((Y - cp.dot(cp.dot(W,A),X))**2)
-            metric=[i,cp.asnumpy(chi2+lam*AA),cp.asnumpy(chi2),lam*AA]
-            logmetric.append(metric)
-            #            print(metric,np.sum(A),np.sum(X))
-            import terminalplot
-            Xn=cp.asnumpy(X)
-            bandl=np.array(range(0,len(Xn[0,:])))
-            terminalplot.plot(list(bandl),list(Xn[np.mod(jj,3),:]))
-
             if np.mod(i,10000)==0:
                 LogNMF(i,cp.asnumpy(A),cp.asnumpy(X))
         #----------------------------------------------------
             
         Wt = cp.dot(cp.dot(W.T,Y),X.T)+ epsilon
-        Wb = (cp.dot(cp.dot(cp.dot(cp.dot(W.T,W),A),X),X.T)) + lam*2*A + epsilon
+        Wb = (cp.dot(cp.dot(cp.dot(cp.dot(W.T,W),A),X),X.T)) + lamA*A + epsilon
         A = A*(Wt/Wb)
         #A = cp.dot(cp.diag(1/cp.sum(A[:,:],axis=1)),A)
         A = cp.dot(cp.diag(1/cp.sum(A[:,:],axis=1)),A)
         
         Wt = cp.dot(cp.dot(A.T,W.T),Y)+ epsilon
-        Wb = cp.dot(cp.dot(cp.dot(cp.dot(A.T,W.T),W),A),X)+ 2000.0*X + epsilon 
+        Wb = cp.dot(cp.dot(cp.dot(cp.dot(A.T,W.T),W),A),X)+ lamX*X + epsilon 
         X = X*(Wt/Wb)
         #X = cp.dot(cp.diag(1/cp.sum(X[:,:],axis=1)),X)
         #X = cp.dot(X,cp.diag(1/cp.sum(X[:,:],axis=0)))
