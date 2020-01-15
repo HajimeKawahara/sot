62dafba3f012b1e6c5993376865193004d417c05
Hajime Kawahara <divrot@gmail.com>
2020-01-16 08:49:19 +0900

    plotmap

--- nmfmap/plotmap.py
+++ nmfmap/plotmap.py
@@ -5,92 +5,54 @@ import matplotlib.pyplot as plt
 import numpy as np
 import healpy as hp
 import matplotlib
-#dat=np.load("npz/T113/T113_L2_A-2.0X-infj99000.npz")
 
-#dat=np.load("npznew/L2-VRLDAX_a-2.0x2.0_try100000j57000.npz")
-dat=np.load("npz/T116/T116_L2-VRDet_A-2.0X2.0j22000.npz")
+def readax(axfile):
+    dat=np.load(axfile)
+    A=dat["arr_0"]
+    X=dat["arr_1"]
+    resall=dat["arr_2"]
 
-dat2=np.load("npz/T114/T114_L2_A-2.0X-infj99000.npz")
+    return A,X,resall
 
-#dat=np.load("npz/T114/T114_L2_A-3.0X-infj42000.npz")
+def getband():
+    bands=[[0.4,0.45],[0.45,0.5],[0.5,0.55],[0.55,0.6],[0.6,0.65],[0.65,0.7],[0.7,0.75],[0.75,0.8],[0.8,0.85],[0.85,0.9]]
+    return bands
 
-bands=[[0.4,0.45],[0.45,0.5],[0.5,0.55],[0.55,0.6],[0.6,0.65],[0.65,0.7],[0.7,0.75],[0.75,0.8],[0.8,0.85],[0.85,0.9]]
-#bands=[[0.4,0.45],[0.45,0.5],[0.5,0.55],[0.55,0.6],[0.6,0.65],[0.65,0.7],[0.7,0.75]]
-
-A=dat["arr_0"]
-X=dat["arr_1"]
-resall=dat["arr_2"]
-
-A2=dat2["arr_0"]
-X2=dat2["arr_1"]
-
-
-#resall.append([res,Like,RA,RX])
-print(np.shape(resall))
-fontsize=18
-matplotlib.rcParams.update({'font.size':fontsize})
-fig=plt.figure(figsize=(10,7))
-s=0
-plt.plot(resall[s:,0],label="$||D-WAX||_F^2/2+R(A,X)$")
-plt.plot(resall[s:,1],label="$||D-WAX||_F^2/2$")
-plt.plot(resall[s:,2],label="$R(A)$")
-plt.plot(resall[s:,3],label="$R(X)$")
-plt.legend()
-plt.yscale("log")
-#plt.xscale("log")
-plt.xlabel("Interation #")
-plt.ylabel("Cost terms")
-
-plt.show()
-
-## load class map
-dataclass=np.load("/home/kawahara/exomap/sot/data/cmap3class.npz")
-cmap=dataclass["arr_0"]
-npix=len(cmap)
-nclass=(len(np.unique(cmap)))
-nside=hp.npix2nside(npix)
-vals=dataclass["arr_1"]
-valexp=dataclass["arr_2"]
-print("Nclass=",nclass)
+def plot_resall(resall):
+    fontsize=18
+    matplotlib.rcParams.update({'font.size':fontsize})
+    fig=plt.figure(figsize=(10,7))
+    s=0
+    plt.plot(resall[s:,0],label="$||D-WAX||_F^2/2+R(A,X)$")
+    plt.plot(resall[s:,1],label="$||D-WAX||_F^2/2$")
+    plt.plot(resall[s:,2],label="$R(A)$")
+    plt.plot(resall[s:,3],label="$R(X)$")
+    plt.legend()
+    plt.yscale("log")
+    plt.xlabel("Interation #")
+    plt.ylabel("Cost terms")
+    plt.show()
 
 
-
-### Set reflectivity
-cloud,cloud_ice,snow_fine,snow_granular,snow_med,soil,veg,ice,water,clear_sky\
-=io_refdata.read_refdata("/home/kawahara/exomap/sot/data/refdata")
-
-#mean albedo between waves and wavee
-#bands=[[0.4,0.5],[0.5,0.6],[0.6,0.7],[0.7,0.8],[0.8,0.9]]#,[0.9,1.0]]
-
-refsurfaces=[water,soil,veg]
-malbedo=io_surface_type.set_meanalbedo(0.8,0.9,refsurfaces,clear_sky)
-mmap,Ain,Xin=toymap.make_multiband_map(cmap,refsurfaces,clear_sky,vals,bands)
-ave_band=np.mean(np.array(bands),axis=1)
-
-cc=plt.cm.viridis
-fontsize=18
-#A=np.log10(A)
-matplotlib.rcParams.update({'font.size':fontsize})
-hp.mollview(A[:,0], title="Component 0",flip="geo",cmap=cc)#,min=0,max=1)
-
-plt.savefig("C0.pdf", bbox_inches="tight", pad_inches=0.0)
-
-hp.mollview(A[:,1], title="Component 1",flip="geo",cmap=cc)#,min=0,max=1)
-plt.savefig("C1.pdf", bbox_inches="tight", pad_inches=0.0)
-
-hp.mollview(A[:,2], title="Component 2",flip="geo",cmap=cc)#,min=0,max=1)
-plt.savefig("C2.pdf", bbox_inches="tight", pad_inches=0.0)
-
-try:
-    hp.mollview(A[:,3], title="Component 3",flip="geo",cmap=cc)#,min=0,max=1)
-    plt.savefig("C3.pdf", bbox_inches="tight", pad_inches=0.0)
-except:
-    print("No 4th comp")
-#hp.mollview(A[:,0]+A[:,1], title="0+1",flip="geo",cmap=plt.cm.jet)
-
-fig= plt.figure(figsize=(10,7))
-ax = fig.add_subplot(111)
-nnl=1#len(np.median(bands,axis=1))
+def load_template():
+    ## load class map
+    dataclass=np.load("/home/kawahara/exomap/sot/data/cmap3class.npz")
+    cmap=dataclass["arr_0"]
+    npix=len(cmap)
+    nclass=(len(np.unique(cmap)))
+    nside=hp.npix2nside(npix)
+    vals=dataclass["arr_1"]
+    valexp=dataclass["arr_2"]
+    print("Nclass=",nclass)
+    
+    #mean albedo between waves and wavee
+    #bands=[[0.4,0.5],[0.5,0.6],[0.6,0.7],[0.7,0.8],[0.8,0.9]]#,[0.9,1.0]]
+    
+    refsurfaces=[water,soil,veg]
+    malbedo=io_surface_type.set_meanalbedo(0.8,0.9,refsurfaces,clear_sky)
+    mmap,Ain,Xin=toymap.make_multiband_map(cmap,refsurfaces,clear_sky,vals,bands)
+    ave_band=np.mean(np.array(bands),axis=1)
+    return 
 
 def norm(arr):
     mask=(arr[:,0]>0.4)*(arr[:,0]<0.9)
@@ -104,72 +66,98 @@ def norm(arr):
     val=val
     return u,val,normv
 
-u,val,normvveg=norm(veg)
-ax.plot(u,val,c="black",lw=2,label="vegitation (deciduous)")
-u,val,normvsoil=norm(soil)
-ax.plot(u,val,c="gray",lw=1,label="soil")
-u,val,normvwater=norm(water)
-ax.plot(u,val,c="gray",ls="-.",label="water")
-plt.xlim(0.4,0.9)
-
-#io_surface_type.plot_albedo(veg,soil,cloud,snow_med,water,clear_sky,ave_band,malbedo,valexp)
-fac=1.0
-mband=np.median(bands,axis=1)
-dband=mband[1]-mband[0]
-fac0=fac/np.sum(X[0,:])/dband/normvveg
-fac1=fac/np.sum(X[1,:])/dband/normvwater
-fac2=fac/np.sum(X[2,:])/dband/normvsoil
-#fac3=fac/np.sum(X[3,:])/dband
-
-plt.plot(np.median(bands,axis=1),X[0,:]*fac0,"o",label="Component 0",color="C0")
-plt.plot(np.median(bands,axis=1),X[1,:]*fac1,"s",label="Component 1",color="C1")
-plt.plot(np.median(bands,axis=1),X[2,:]*fac2,"^",label="Component 2",color="C2")
-
-try:
-    plt.plot(np.median(bands,axis=1),X[3,:]*fac3,"^",label="Component 3",color="C3")
-    plt.plot(np.median(bands,axis=1),X[3,:]*fac3,color="C3")
+def moll(A):
+    cc=plt.cm.viridis
+    fontsize=18
+    matplotlib.rcParams.update({'font.size':fontsize})
+    hp.mollview(A[:,0], title="Component 0",flip="geo",cmap=cc)#,min=0,max=1)
     
-except:
-    print("No 4th comp")
-#plt.plot(np.median(bands,axis=1),(X[0,:]+X[1,:])*fac3,"^",label="Component 0+1",color="C1")
+    plt.savefig("C0.pdf", bbox_inches="tight", pad_inches=0.0)
+    
+    hp.mollview(A[:,1], title="Component 1",flip="geo",cmap=cc)#,min=0,max=1)
+    plt.savefig("C1.pdf", bbox_inches="tight", pad_inches=0.0)
+    
+    hp.mollview(A[:,2], title="Component 2",flip="geo",cmap=cc)#,min=0,max=1)
+    plt.savefig("C2.pdf", bbox_inches="tight", pad_inches=0.0)
+    
+    try:
+        hp.mollview(A[:,3], title="Component 3",flip="geo",cmap=cc)#,min=0,max=1)
+        plt.savefig("C3.pdf", bbox_inches="tight", pad_inches=0.0)
+    except:
+        print("No 4th comp")
+        #hp.mollview(A[:,0]+A[:,1], title="0+1",flip="geo",cmap=plt.cm.jet)
 
-plt.plot(np.median(bands,axis=1),X[0,:]*fac0,color="C0")
-plt.plot(np.median(bands,axis=1),X[1,:]*fac1,color="C1")
-plt.plot(np.median(bands,axis=1),X[2,:]*fac2,color="C2")
-#plt.plot(np.median(bands,axis=1),X2[0,:]*fac0,color="C0")
-#plt.plot(np.median(bands,axis=1),X2[1,:]*fac1,color="C1")
-#plt.plot(np.median(bands,axis=1),X2[2,:]*fac2,color="C2")
+def plref(X,bands):
+    cloud,cloud_ice,snow_fine,snow_granular,snow_med,soil,veg,ice,water,clear_sky=io_refdata.read_refdata("/home/kawahara/exomap/sot/data/refdata")
+    fig= plt.figure(figsize=(10,7))
+    ax = fig.add_subplot(111)
+    nnl=1#len(np.median(bands,axis=1))
+    u,val,normvveg=norm(veg)
+    ax.plot(u,val,c="black",lw=2,label="vegitation (deciduous)")
+    u,val,normvsoil=norm(soil)
+    ax.plot(u,val,c="gray",lw=1,label="soil")
+    u,val,normvwater=norm(water)
+    ax.plot(u,val,c="gray",ls="-.",label="water")
+    plt.xlim(0.4,0.9)
+    fac=1.0
+    mband=np.median(bands,axis=1)
+    dband=mband[1]-mband[0]
+    fac0=fac/np.sum(X[0,:])/dband/normvveg
+    fac1=fac/np.sum(X[1,:])/dband/normvwater
+    fac2=fac/np.sum(X[2,:])/dband/normvsoil
+    #fac3=fac/np.sum(X[3,:])/dband
+    
+    plt.plot(np.median(bands,axis=1),X[0,:]*fac0,"o",label="Component 0",color="C0")
+    plt.plot(np.median(bands,axis=1),X[1,:]*fac1,"s",label="Component 1",color="C1")
+    plt.plot(np.median(bands,axis=1),X[2,:]*fac2,"^",label="Component 2",color="C2")
+    
+    try:
+        plt.plot(np.median(bands,axis=1),X[3,:]*fac3,"^",label="Component 3",color="C3")
+        plt.plot(np.median(bands,axis=1),X[3,:]*fac3,color="C3")
+        
+    except:
+        print("No 4th comp")
+        
+    plt.plot(np.median(bands,axis=1),X[0,:]*fac0,color="C0")
+    plt.plot(np.median(bands,axis=1),X[1,:]*fac1,color="C1")
+    plt.plot(np.median(bands,axis=1),X[2,:]*fac2,color="C2")
+    
+    plt.tick_params(labelsize=16)
+    plt.ylabel("Reflection Spectra",fontsize=16)
+    plt.xlabel("wavelength [micron]",fontsize=16)
+    plt.legend(fontsize=13)
+    plt.title("Unconstrained")
+    plt.savefig("ref.pdf", bbox_inches="tight", pad_inches=0.0)
 
-#plt.plot(np.median(bands,axis=1),(X[0,:]+X[1,:])*fac3,color="C1")
-
-plt.tick_params(labelsize=16)
-plt.ylabel("Reflection Spectra",fontsize=16)
-plt.xlabel("wavelength [micron]",fontsize=16)
-plt.legend(fontsize=13)
-plt.title("Unconstrained")
-plt.savefig("ref.pdf", bbox_inches="tight", pad_inches=0.0)
-
-#A[(A>0.333333)*(A<0.333334)]=0
-#A[:,0]=A[:,0]*-1.0
-#A[:,3]=0.0
-
-#A[:,0]=0.0
-#A[:,1]=10.0*A[:,1]
-
-Aclass=np.argmax(A,axis=1)
-print(Aclass)
-Aclass[Aclass==0]=70
-Aclass[Aclass==1]=100
-Aclass[Aclass==2]=0
-#Aclass[Aclass==3]=30
-
-hp.mollview(Aclass, title="Retrieved",flip="geo",cmap=plt.cm.Greys,max=100)
-plt.savefig("retrieved.pdf", bbox_inches="tight", pad_inches=0.0)
+def classmap(A):
+    Aclass=np.argmax(A,axis=1)
+    Aclass[Aclass==0]=70
+    Aclass[Aclass==1]=100
+    Aclass[Aclass==2]=0
+    #Aclass[Aclass==3]=30
+    
+    hp.mollview(Aclass, title="Retrieved",flip="geo",cmap=plt.cm.Greys,max=100)
+    plt.savefig("retrieved.pdf", bbox_inches="tight", pad_inches=0.0)
 
 
-dataclass=np.load("/home/kawahara/exomap/sot/data/cmap3class.npz")
-cmapans=dataclass["arr_0"]
-hp.mollview(cmapans, title="Input",flip="geo",cmap=plt.cm.Greys_r)
-plt.savefig("input.pdf", bbox_inches="tight", pad_inches=0.0)
+def inmap():
+    dataclass=np.load("/home/kawahara/exomap/sot/data/cmap3class.npz")
+    cmapans=dataclass["arr_0"]
+    hp.mollview(cmapans, title="Input",flip="geo",cmap=plt.cm.Greys_r)
+    plt.savefig("input.pdf", bbox_inches="tight", pad_inches=0.0)
+    plt.show()
 
-plt.show()
+if __name__=='__main__':
+
+    axfile="npz/T116/T116_L2-VRLD_A-2.0X4.0j64000.npz"
+    A,X,resall=readax(axfile)
+    bands=getband()
+    plot_resall(resall)    
+    moll(A)
+    plref(X,bands)
+    classmap(A)
+    inmap()
+
+
+
+
