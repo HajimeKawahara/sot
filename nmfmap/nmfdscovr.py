f0bbb03de8612f70f214380b3be55089616c5ce2
Hajime Kawahara <divrot@gmail.com>
2020-02-01 21:01:34 +0900

    semi NMF

--- nmfmap/nmfdscovr.py
+++ nmfmap/nmfdscovr.py
@@ -18,18 +18,19 @@ np.random.seed(34)
 ## load DSCOVR
 nside=16
 npix=hp.nside2npix(nside)
-W,t,lcall=rd.read_dscovr("/home/kawahara/exomap/data/for_HKawahara",4,istart=1)
+W,t,lcall=rd.read_dscovr("/home/kawahara/exomap/data/for_HKawahara",4,istart=3)
 lcall=lcall*npix
 lcall=lcall/6
 #print(np.std(lcall)**2*np.shape(lcall)[0]*np.shape(lcall)[1])
 #np.savez("lcdscovr",lcall)
 ##################################################
 N=3
-lamA=1.e-3 #-6 sparse
-lamX=1.e1
+lamA=10**(-2.0) #-6 sparse
+lamX=10**(-10.0)
 Ntry=100000
 epsilon=1.e-12
-trytag="D126x"
+trytag="Ds201"
+semiNMF=True
 regmode="L2-VRDet"
 #regmode="L2-VRLD"
 #regmode="Dual-L2"
@@ -37,11 +38,11 @@ regmode="L2-VRDet"
 filename=trytag+regmode+"_A"+str(np.log10(lamA))+"X"+str(np.log10(lamX))
 
 A0,X0=initnmf.init_random(N,npix,lcall)
-X0=X0/10
+#X0=X0/10
 #A0,X0=initnmf.initpca(N,W,lcall,lamA,mode="Ridge")
 #fac=np.sum(lcall)/np.sum(A0)/np.sum(X0)
 #A0=A0*fac
-A,X,logmetric=runnmf.QP_NMR(regmode,Ntry,lcall,W,A0,X0,lamA,lamX,epsilon,filename,NtryAPGX=100,NtryAPGA=1000,eta=1.e-12,endc=-np.inf,Nsave=1000)
+A,X,logmetric=runnmf.QP_NMR(regmode,Ntry,lcall,W,A0,X0,lamA,lamX,epsilon,filename,NtryAPGX=100,NtryAPGA=1000,eta=1.e-12,endc=-np.inf,Nsave=1000,semiNMF=semiNMF)
 np.savez(filename,A,X)
 
 #plt.show()
