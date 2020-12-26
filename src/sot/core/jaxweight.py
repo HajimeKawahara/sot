d9172006519bb0319dcba5a02068412bc6d63028
Hajime Kawahara <divrot@gmail.com>
2020-12-26 21:10:39 +0900

    added JAX ADAM optimizer for L2 SOT

--- src/sot/core/jaxweight.py
+++ src/sot/core/jaxweight.py
@@ -10,6 +10,8 @@ import time
 import scipy.signal
 from scipy.interpolate import splev, splrep
 import tqdm
+import jax.numpy as jnp
+from jax import jit
 
 def comp_omega(nside):
     omega = []
@@ -18,53 +20,49 @@ def comp_omega(nside):
     for ipix in range(0, npix):
         theta, phi = hp.pix2ang(nside, ipix)
         omega.append([theta, phi])
-    return np.array(omega)
+    return jnp.array(omega)
 
-
+@jit
 def uniteO(inc, Thetaeq):
     # (3)
-    eO = np.array([np.sin(inc)*np.cos(Thetaeq), -
-                   np.sin(inc)*np.sin(Thetaeq), np.cos(inc)])
+    eO = jnp.array([jnp.sin(inc)*jnp.cos(Thetaeq), -
+                   jnp.sin(inc)*jnp.sin(Thetaeq), jnp.cos(inc)])
     return eO
 
-
+@jit
 def uniteS(Thetaeq, Thetav):
     # (3,nsamp)
-    eS = np.array(
-        [np.cos(Thetav-Thetaeq), np.sin(Thetav-Thetaeq), np.zeros(len(Thetav))])
+    eS = jnp.array(
+        [jnp.cos(Thetav-Thetaeq), jnp.sin(Thetav-Thetaeq), jnp.zeros(len(Thetav))])
     return eS
 
-
+@jit
 def uniteR(zeta, Phiv, omega):
     # (3,nsamp,npix)
-    np.array([Phiv]).T
-    costheta = np.cos(omega[:, 0])
-    sintheta = np.sin(omega[:, 0])
-    cosphiPhi = np.cos(omega[:, 1]+np.array([Phiv]).T)
-    sinphiPhi = np.sin(omega[:, 1]+np.array([Phiv]).T)
+    jnp.array([Phiv]).T
+    costheta = jnp.cos(omega[:, 0])
+    sintheta = jnp.sin(omega[:, 0])
+    cosphiPhi = jnp.cos(omega[:, 1]+jnp.array([Phiv]).T)
+    sinphiPhi = jnp.sin(omega[:, 1]+jnp.array([Phiv]).T)
 #    cosphiPhi=np.cos(omega[:,1]-np.array([Phiv]).T)
 #    sinphiPhi=np.sin(omega[:,1]-np.array([Phiv]).T)
 
     x = cosphiPhi*sintheta
-    y = np.cos(zeta)*sinphiPhi*sintheta+np.sin(zeta)*costheta
-    z = -np.sin(zeta)*sinphiPhi*sintheta+np.cos(zeta)*costheta
-    eR = np.array([x, y, z])
+    y = jnp.cos(zeta)*sinphiPhi*sintheta+jnp.sin(zeta)*costheta
+    z = -jnp.sin(zeta)*sinphiPhi*sintheta+jnp.cos(zeta)*costheta
+    eR = jnp.array([x, y, z])
 
     return eR
 
+@jit
 def comp_weight(nside,zeta,inc,Thetaeq,Thetav,Phiv):
-    """
-    Summary
-    ------------
-    Compute weight functions W_illuminated and W_visible
-    
-    """
     omega=comp_omega(nside)
     eO=uniteO(inc,Thetaeq)
     eS=uniteS(Thetaeq,Thetav)
     eR=uniteR(zeta,Phiv,omega)
-    WV=np.einsum("ijk,i->jk",eR,eO)
-    WV[WV<0.0]=0.0
-    WI=np.einsum("ijk,ij->jk",eR,eS)
-    WI[WI<0.0]=0.0
+    WV=jnp.einsum("ijk,i->jk",eR,eO)
+    WV=jnp.where(WV<0.0,0.0,WV)
+    WI=jnp.einsum("ijk,ij->jk",eR,eS)
+    WI=jnp.where(WI<0.0,0.0,WI)
     return WI,WV
+
