3edf0cceafd84a94db1c30d5ae461cea9d94ba7d
Hajime Kawahara <divrot@gmail.com>
2025-04-13 10:47:46 +0900

    rottheory in jax

--- src/sot/misc/rottheory.py
+++ src/sot/misc/rottheory.py
@@ -1,24 +1,33 @@
-"""reproduces Figure 2 in Kawahara (2016).
-"""
+"""reproduces Figure 2 in Kawahara (2016)."""
 
-import argparse
-import numpy as np
+import jax.numpy as jnp
 
+def modulation_factor_max(Theta, zeta, inc, Thetaeq):
+    """computes the modulation factor at maximum weight, defined in Kawahara (2016)
 
-def modmax(Theta, zeta, inc, Thetaeq):
+    Args:
+        Theta (float): phase angle (radian)
+        zeta (float): obliquity angle (radian)
+        inc (float)): inclination angle (radian)
+        Thetaeq (float): phase angle at equinox (radian)
+
+    Returns:
+        float: modulation factor at maximum weight
+    """
+
     fac1 = (
-        -np.cos(zeta)
-        + np.cos(inc) * np.sin(zeta) * np.sin(Theta - Thetaeq)
-        - np.cos(zeta) * np.cos(Theta) * np.sin(inc)
+        -jnp.cos(zeta)
+        + jnp.cos(inc) * jnp.sin(zeta) * jnp.sin(Theta - Thetaeq)
+        - jnp.cos(zeta) * jnp.cos(Theta) * jnp.sin(inc)
     )
     fac2 = (
-        np.cos(Theta - Thetaeq) ** 2
-        + 2 * np.cos(Theta - Thetaeq) * np.cos(Thetaeq) * np.sin(inc)
-        + np.cos(Thetaeq) ** 2 * np.sin(inc) ** 2
+        jnp.cos(Theta - Thetaeq) ** 2
+        + 2 * jnp.cos(Theta - Thetaeq) * jnp.cos(Thetaeq) * jnp.sin(inc)
+        + jnp.cos(Thetaeq) ** 2 * jnp.sin(inc) ** 2
         + (
-            np.cos(inc) * np.sin(zeta)
-            - np.cos(zeta) * np.sin(Theta - Thetaeq)
-            + np.cos(zeta) * np.sin(inc) * np.sin(Thetaeq)
+           jnp.cos(inc) * jnp.sin(zeta)
+            - jnp.cos(zeta) * jnp.sin(Theta - Thetaeq)
+            + jnp.cos(zeta) * jnp.sin(inc) * jnp.sin(Thetaeq)
         )
         ** 2
     )
@@ -27,6 +36,7 @@ def modmax(Theta, zeta, inc, Thetaeq):
 
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
+    import argparse
 
     parser = argparse.ArgumentParser(description="max approx")
     parser.add_argument(
@@ -36,14 +46,16 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     N = 180
-    gTheta = np.linspace(0, 2 * np.pi, 2 * N)
-    gzeta = np.linspace(0, np.pi, N)
-    X, Y = np.meshgrid(gTheta, gzeta)
-    gridT = gTheta * np.array([np.ones(N)]).T
-    gridz = (gzeta * np.array([np.ones(2 * N)]).T).T
+    gTheta = jnp.linspace(0, 2 * jnp.pi, 2 * N)
+    gzeta = jnp.linspace(0, jnp.pi, N)
+    X, Y = jnp.meshgrid(gTheta, gzeta)
+    gridT = gTheta * jnp.array([jnp.ones(N)]).T
+    gridz = (gzeta * jnp.array([jnp.ones(2 * N)]).T).T
 
-    arr = modmax(gridT, gridz, args.i[0] / 180 * np.pi, args.t[0] / 180 * np.pi)
-    print(np.shape(arr))
+    arr = modulation_factor_max(
+        gridT, gridz, args.i[0] / 180 * jnp.pi, args.t[0] / 180 * jnp.pi
+    )
+    print(jnp.shape(arr))
     fig = plt.figure()
     plt.contour(
         X,
@@ -53,7 +65,7 @@ if __name__ == "__main__":
         levels=[-1.25, -1.0, -0.75, -0.5, -0.25, 0, 0.25, 0.5, 0.75, 1.0, 1.25],
     )
     plt.imshow(
-        arr, cmap="coolwarm", vmin=-1.0, vmax=1.0, extent=[0, 2 * np.pi, np.pi, 0]
+        arr, cmap="coolwarm", vmin=-1.0, vmax=1.0, extent=[0, 2 * jnp.pi, jnp.pi, 0]
     )
     plt.gca().invert_yaxis()
     plt.ylabel("$\zeta$", fontsize=15)
